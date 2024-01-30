54a2d21813f793c509314f62630e39ea5ab493dd
fls0 <134416720+fls0@users.noreply.github.com>
2024-01-30 15:46:29 +0100

    upd

--- Shop/core/models.py
+++ Shop/core/models.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(null=False)
+    description = models.CharField(max_length=255, blank=True)
+
+    created_at = models.DateTimeField(default=datetime.now(), blank=True)
+
+
