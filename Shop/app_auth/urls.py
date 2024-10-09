3b175b6992c12724bb8159cf8b93532f8cd5edf2
fls0 <134416720+fls0@users.noreply.github.com>
2024-10-09 14:28:45 +0200

    upd

--- Shop/app_auth/urls.py
+++ Shop/app_auth/urls.py
@@ -9,7 +9,7 @@ app_name = "app_auth"
 urlpatterns = [
     path('signup/', views.RegisterView.as_view(), name='signup'),
     path('signin/',
-         LoginView.as_view(template_name='app_auth/login.html', form_class=LoginForm, redirect_authenticated_user=True),
-         name='signin'),
+         LoginView.as_view(template_name='app_auth/login.html', form_class=LoginForm, redirect_authenticated_user=True,
+                           next_page='core:core'), name='signin'),
     path('logout/', LogoutView.as_view(template_name='app_auth/logout.html'), name='logout')
 ]
